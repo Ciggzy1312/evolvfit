db7a3bc997dcf91e346a6eb8eb9f84947da4341d
Deepayan <deepayanmukherjee1312@gmail.com>
2022-07-26 19:17:30 +0530

    Added optimizedMeal controller
    Signed-off-by: Deepayan <deepayanmukherjee1312@gmail.com>

--- controllers/foodItem.js
+++ controllers/foodItem.js
@@ -27,4 +27,25 @@ const createFoodItem = async (req,res)=>{
     }
 }
 
-module.exports = { createFoodItem };
+
+const optimizedMeal = async (req,res)=>{
+
+    const { calories } = req.body;
+
+    try {
+        
+        const item = await FoodItem.find().where('calories').gt(calories-100).lt(calories+100)
+                                        .where('protein').gt((calories*20)/100).lt((calories*30)/100)
+
+        if(item.length>=1 && item.length<5){
+            res.status(200).json(item)
+        } else{
+            res.status(200).json({message: "No items found for this criteria"})
+        }
+
+    } catch (error) {
+        res.status(400).json(error)
+    }
+}
+
+module.exports = { createFoodItem, optimizedMeal };
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router()
 const { createUser, createUserMeal, updateUserMeal } = require('../controllers/user');
+const { optimizedMeal } = require('../controllers/foodItem');
 
 router.post("/", createUser)
 router.post("/:id", createUserMeal)
 router.patch("/:id/:mealIndex", updateUserMeal)
+
+router.get("/optimizedmeal", optimizedMeal)
 
 module.exports = router;
